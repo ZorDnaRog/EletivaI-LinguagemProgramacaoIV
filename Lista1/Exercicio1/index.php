b81351ee763c5fb187cfcad24cf857bd52e5a4a8
wgsteixeira <99516056+wgsteixeira@users.noreply.github.com>
2022-03-04 22:22:07 -0300

    Update index.php

--- Lista1/Exercicio1/index.php
+++ Lista1/Exercicio1/index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,32 +9,32 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercicio 01</title>
-  </head>
+    <title>Lista de Exercicios 1 - Troco</title>
+</head>
 
 <body class="container">
-  <h1>Exercicio 01</h1>
+    <h1>Programa para calcular o troco</h1>
 
-  <form action="resultado.php" method="post">
-      <div class="row">
-          <div class="col">
-              <label for="valorpago" class="label-control">Informe o valor pago: </label>
-              <input type="number" name="valorpago" id="valorpago" class="form-control" />
-          </div>
-          <div class="col">
-              <label for="precoproduto" class="label-control">Informe preço do produto: </label>
-              <input type="number" name="precoproduto" id="precoproduto" class="form-control" />
-          </div>
-      </div>
-      <div class="row mt-5">
-          <div class="col">
-              <button type="submit" class="btn btn-danger">
-                  Enviar
-              </button>
-          </div>
-      </div>
+    <form action="resultado.php" method="post">
+        <div class="row">
+            <div class="col">
+                <label for="valorpago" class="label-control">Informe o valor pago: </label>
+                <input type="number" name="valorpago" id="valorpago" class="form-control" />
+            </div>
+            <div class="col">
+                <label for="precoproduto" class="label-control">Informe o preço do produto: </label>
+                <input type="numer" name="precoproduto" id="precoproduto" class="form-control" />
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <button type="submit" class="btn btn-danger">
+                    Enviar
+                </button>
+            </div>
+        </div>
 
-  </form>
+    </form>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -45,5 +46,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
