23ed3dd14f91b210c8112e984a04564e8d058096
wgsteixeira <wgsteixeira@outlook.com>
2022-03-15 14:47:53 -0300

    Update resultado.php
    Resolução Exercício

--- Lista1/Exercicio3/resultado.php
+++ Lista1/Exercicio3/resultado.php
@@ -1,4 +1,12 @@
 <?php 
     $valor = $_POST['valorverificar'];
-    $valor
+    $valorparametro = doubleval($valor);
+    if ($valorparametro >10){
+        echo "Valor maior que 10";
+    } else if($valorparametro < 10){
+        echo "Valor menor que 10";
+    } else{
+        echo "Valor é igual a 10";
+    }
+?>
     
