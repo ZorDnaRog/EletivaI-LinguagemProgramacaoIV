738df7534f3bdef5ba2dcac63d250bcf37d10c81
wgsteixeira <99516056+wgsteixeira@users.noreply.github.com>
2022-03-23 20:41:52 -0300

    Realização de Exercicios
    Formatação do código de exercícios. Resolução do Exercício 04.

---
+++ Lista1/Exercicio4/resultado.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Lista de Exercicios 01</title>
+  </head>
+  <body>
+    <h1>Resposta do Exercicio 04</h1>
+
+    <?php 
+        $numero = $_POST['verificarnumero'];
+        
+    
+    ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
