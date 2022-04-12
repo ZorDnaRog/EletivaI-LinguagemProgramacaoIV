a21f9f63ff1219e17d3ca0bd9492a152160109c9
wgsteixeira <wgsteixeira@outlook.com>
2022-04-12 17:15:05 -0300

    Resolução Exercicio 04

--- Lista4/Exercicio1/index.php
+++ Lista4/Exercicio1/index.php
@@ -1,33 +1,47 @@
 <!doctype html>
 <html lang="en">
-  <head>    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Lista de Exericicios 04</title>
-  </head>
-  <body class="container mt-3">
-    <h1>Exercicio 01</h1>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <form action="" method="POST">
+  <title>Lista de Exericicios 04</title>
+</head>
+
+<body class="container mt-3">
+  <h1>Exercicio 01</h1>
+
+  <form action="resultado.php" method="POST">
     <div class="col-5 container border mt-5">
-    <h3>Calculo do Tempo</h3>
-    <div class="row container mt-3">
-      <div class="col-5">        
-      <label for="tempo" class="label-control">Tempo da Volta</label>
-        <input type="time" name="tempo" id="tempo" class="form-control" required>      
-      </div>      
-      <div class="row mt-3">  
-      <div class="col mb-3" style="text-align: left">
-        <button type="submit" name="inserir" class="col btn btn-danger">Inserir Tempo</button>
-        <button type="submit" name="exibir" class="col btn btn-success" type="button">Exibir Resultados</button>
-      </div>     
-    </div>
-    </div>
-    </form>
-        
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-  </body>
+      <h3>Calculo do Tempo</h3>
+      <div class="row container mt-3">
+        <div class="col-5">
+          <label for="tempo1" class="label-control">Tempo da Volta 1</label>
+          <input type="number" name="tempo1" id="tempo1" class="form-control">
+        </div>
+        <div class="col-5">
+          <label for="tempo2" class="label-control">Tempo da Volta 2</label>
+          <input type="number" name="tempo2" id="tempo2" class="form-control">
+        </div>
+        <div class="row mt-3">
+          <div class="col mb-3">
+            <button type="submit" class="col btn btn-danger">Inserir Tempo</button>            
+          </div>
+        </div>
+      </div>
+  </form>
+
+  <?php
+
+      session_start();
+
+
+  ?>
+  
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
 </html>
---
+++ Lista4/Exercicio1/voltas.php
@@ -0,0 +1,13 @@
+<?php
+
+    session_start();
+
+    echo $_SESSION['volta1'];
+    echo $_SESSION['volta2'];
+ 
+
+
+?>  
+
+<br>
+<button class="btn btn-primary" onclick="window.location.href = 'index.php'">Inserir Outro Tempo</button>
