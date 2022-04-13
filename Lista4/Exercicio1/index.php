b8799f0ee11f157bb404a66f09613b4259cebab9
wgsteixeira <99516056+wgsteixeira@users.noreply.github.com>
2022-04-12 23:02:13 -0300

    Commit to main

--- Lista4/Exercicio1/index.php
+++ Lista4/Exercicio1/index.php
@@ -12,25 +12,38 @@
 <body class="container mt-3">
   <h1>Exercicio 01</h1>
 
-  <form action="resultado.php" method="POST">
+  <form action="" method="POST">
     <div class="col-5 container border mt-5">
       <h3>Calculo do Tempo</h3>
       <div class="row container mt-3">
         <div class="col-5">
-          <label for="tempo1" class="label-control">Tempo da Volta 1</label>
-          <input type="number" name="tempo1" id="tempo1" class="form-control">
-        </div>
-        <div class="col-5">
-          <label for="tempo2" class="label-control">Tempo da Volta 2</label>
-          <input type="number" name="tempo2" id="tempo2" class="form-control">
-        </div>
+          <label for="tempo" class="label-control">Tempo da Volta</label>
+          <input type="text" name="tempo" id="tempo" class="form-control">
+        </div>        
         <div class="row mt-3">
           <div class="col mb-3">
             <button type="submit" class="col btn btn-danger">Inserir Tempo</button>            
           </div>
+          <div class="col mb-3">
+            <button type="submit" class="col btn btn-primary" name="exibir">Exibir Resultados</button>            
+          </div>
         </div>
       </div>
   </form>
+
+  <?php  
+
+  if(isset($_POST['exibir'])){
+
+    session_start();
+    
+    echo $_SESSION[] = $_POST['tempo'];
+    
+  }
+
+
+
+  ?>
   
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
